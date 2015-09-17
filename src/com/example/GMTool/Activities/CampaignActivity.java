4220af5f44852ccacbd7769a05b6fbe05c98ed0b
Stephan <menzs2@bfh.ch>
2015-09-17 21:49:15 +0200

    Set texts

--- src/com/example/GMTool/Activities/CampaignActivity.java
+++ src/com/example/GMTool/Activities/CampaignActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.GMTool.R;
 
@@ -11,10 +12,14 @@ import com.example.GMTool.R;
  * Created by Thunderchild on 11.09.2015.
  */
 public class CampaignActivity extends Activity {
+
+    private String campaingName = "Campaign Name";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign);
+        TextView detail = (TextView)findViewById(R.id.campaignName);
+        detail.setText(campaingName);
     }
 
     public void startSelectNPCActivity(View view){
--- src/com/example/GMTool/Activities/MainActivity.java
+++ src/com/example/GMTool/Activities/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.example.GMTool.R;
@@ -19,12 +21,34 @@ public class MainActivity extends Activity {
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;//super.onCreateOptionsMenu(menu);
     }
 
     public void openCampaignActivity(View view){
         Intent intent = new Intent(this, CampaignActivity.class);
         startActivity(intent);
+    }
+
+    public boolean  onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+
+            case R.id.action_refresh:
+                Toast.makeText(getApplicationContext(), "not implemented yet",
+                        Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.newCampaignButton:
+                Toast.makeText(getApplicationContext(), "not implemented yet",
+                        Toast.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+
+
+        }
+        return true;
+
     }
 }
